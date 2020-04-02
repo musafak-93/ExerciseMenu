807ed8900bde19b5731cd22ae1a47fb0cac0b6a6
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-02 09:57:53 +0700

    add setonclicklistener untuk function button pindah ke menu hitung

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edname, edpass;
@@ -21,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         edname = findViewById(R.id.Et_Username);
         edpass = findViewById(R.id.Et_password);
         bkirim = findViewById(R.id.btnok);
+
+        bkirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = edname.getText().toString();
+                pass = edpass.getText().toString();
+
+                if (user.equals("admin") || pass.equals("123") ) {
+                    Intent intent = new Intent(MainActivity.this, Menu_Hitung.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),
+                            "Login Gagal", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void Pindah(View view) {
