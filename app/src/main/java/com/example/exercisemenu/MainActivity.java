552db8e4a9f4cfa439bd50b3f0ddac57fe830722
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-02 09:54:06 +0700

    add function pindah untuk pindah ke activity menu hitung

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.exercisemenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
         edname = findViewById(R.id.Et_Username);
         edpass = findViewById(R.id.Et_password);
         bkirim = findViewById(R.id.btnok);
+    }
+
+    public void Pindah(View view) {
+        Intent intent = new Intent(MainActivity.this, Menu_Hitung.class);
+        startActivity(intent);
     }
 }
