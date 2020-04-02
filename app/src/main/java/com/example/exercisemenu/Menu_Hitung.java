92b4357d7da1b4103bf2d07811cecbf3d0187ac6
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-03 01:12:54 +0700

    add popupmenu di java yang digunakan saat textview di klik maka akan keluar 2 menu

--- app/src/main/java/com/example/exercisemenu/Menu_Hitung.java
+++ app/src/main/java/com/example/exercisemenu/Menu_Hitung.java
@@ -3,12 +3,32 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Menu_Hitung extends AppCompatActivity {
+public class Menu_Hitung extends AppCompatActivity implements
+        PopupMenu.OnMenuItemClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__hitung);
+
+        TextView persegi=findViewById(R.id.TV_persegi);
+
+        persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu ppMenu = new PopupMenu(Menu_Hitung.this, v);
+                ppMenu.setOnMenuItemClickListener(Menu_Hitung.this);
+
+                ppMenu.inflate(R.menu.menuhitung);
+                ppMenu.show();
+            }
+        });
     }
 }
