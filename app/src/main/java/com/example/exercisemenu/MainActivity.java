52640a0bd0bd1c526fad7c665ae3304c95bfab9b
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-02 09:49:54 +0700

    add variable dari activity main di mainactivity java untuk login user

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edname, edpass;
+    Button bkirim;
+    String user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edname = findViewById(R.id.Et_Username);
+        edpass = findViewById(R.id.Et_password);
+        bkirim = findViewById(R.id.btnok);
     }
 }
