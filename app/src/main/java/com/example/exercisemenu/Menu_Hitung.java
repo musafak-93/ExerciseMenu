34a1358c02c716313e3103305dcbae0d6ed4115f
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-02 09:16:21 +0700

    Create New Activity Menu Hitung

---
+++ app/src/main/java/com/example/exercisemenu/Menu_Hitung.java
@@ -0,0 +1,14 @@
+package com.example.exercisemenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Hitung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__hitung);
+    }
+}
