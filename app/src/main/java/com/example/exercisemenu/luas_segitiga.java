115c673fb449f4e27d877fdf16205f28b8870185
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-04 09:25:58 +0700

    add proses menghitung luas segitiga di luas_segitiga.java

--- app/src/main/java/com/example/exercisemenu/luas_segitiga.java
+++ app/src/main/java/com/example/exercisemenu/luas_segitiga.java
@@ -3,12 +3,53 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class luas_segitiga extends AppCompatActivity {
+
+    EditText a,t;
+    Button hitung;
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luas_segitiga);
+
+        a = (EditText)findViewById(R.id.Et_alas);
+        t = (EditText)findViewById(R.id.Et_tinggi);
+        hitung = (Button) findViewById(R.id.btn_Luas_segitiga);
+        hasil = (TextView)findViewById(R.id.hsl_luas_segitiga);
+        hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //jika tidak mengisi panjang dan lebar sekaligus maka akan tampil notifikasi
+                if(a.length()==0 && t.length()==0){
+                    Toast.makeText(getApplication(),"Alas dan Tinggi tidak boleh Kosong",Toast.LENGTH_LONG).show();
+                }
+                //notifikasi ini akan muncul jika panjangnya tidak diisi
+                else if (a.length()==0){
+                    Toast.makeText(getApplication(),"Alas tidak boleh kososng", Toast.LENGTH_LONG).show();
+                }
+                //notifikasi jika lebar tidak diisi
+                else if (t.length()==0){
+                    Toast.makeText(getApplication(),"Tinggi tidak boleh kosong",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    String alas = a.getText().toString();
+                    String tinggi = t.getText().toString();
+                    double a = Double.parseDouble(alas);
+                    double t = Double.parseDouble(tinggi);
+                    double hs = LuasSegitiga(a,t);
+                    String output = String.valueOf(hs);
+                    hasil.setText(output.toString());
+                }
+            }
+        });
     }
+    public double LuasSegitiga(double a, double t){return a*t/2;}
 }
