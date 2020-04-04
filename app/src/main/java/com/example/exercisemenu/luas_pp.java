25fdb19f88504c1b459d222cf87544348ad94ad2
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-04 07:57:35 +0700

    add proses menghitung luas persegi panjang di luas_pp.java

--- app/src/main/java/com/example/exercisemenu/luas_pp.java
+++ app/src/main/java/com/example/exercisemenu/luas_pp.java
@@ -3,12 +3,54 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class luas_pp extends AppCompatActivity {
+
+    EditText p,l;
+    Button hitung;
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luas_pp);
+
+        p = (EditText)findViewById(R.id.Et_panjang);
+        l = (EditText)findViewById(R.id.Et_lebar);
+
+        hitung = (Button) findViewById(R.id.btn_luas_persegi_panjang);
+        hasil = (TextView)findViewById(R.id.hsl_luas_persegi_panjang);
+        hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //jika tidak mengisi panjang dan lebar sekaligus maka akan tampil notifikasi
+                if(p.length()==0 && l.length()==0){
+                    Toast.makeText(getApplication(),"Panjang dan lebar tidak boleh Kosong",Toast.LENGTH_LONG).show();
+                }
+                //notifikasi ini akan muncul jika panjangnya tidak diisi
+                else if (p.length()==0){
+                    Toast.makeText(getApplication(),"Panjang tidak boleh kosong", Toast.LENGTH_LONG).show();
+                }
+                //notifikasi jika lebar tidak diisi
+                else if (l.length()==0){
+                    Toast.makeText(getApplication(),"Lebar tidak boleh kosong",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    String isipanjang = p.getText().toString();
+                    String isilebar = l.getText().toString();
+                    double p = Double.parseDouble(isipanjang);
+                    double l = Double.parseDouble(isilebar);
+                    double hs = LuasPersegiPanjang(p,l);
+                    String output = String.valueOf(hs);
+                    hasil.setText(output.toString());
+                }
+            }
+        });
     }
+    public double LuasPersegiPanjang(double p, double l){return p*l;}
 }
