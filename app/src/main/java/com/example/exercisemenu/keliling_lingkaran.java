c876333dfe7ecf34ac01df26791126c01642e7cb
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-04 09:10:40 +0700

    add proses menghitung keliling lingkaran di keliling_lingkaran.java

--- app/src/main/java/com/example/exercisemenu/keliling_lingkaran.java
+++ app/src/main/java/com/example/exercisemenu/keliling_lingkaran.java
@@ -3,12 +3,42 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class keliling_lingkaran extends AppCompatActivity {
+
+    EditText jari;
+    Button hitung;
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_lingkaran);
+
+        jari = (EditText) findViewById(R.id.Et_keliling_jari);
+        hitung = (Button) findViewById(R.id.btn_klingkaran);
+        hasil = (TextView) findViewById(R.id.hsl_keliling_lingkaran);
+
+        hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(jari.length()==0){
+                    Toast.makeText(getApplication(),"Jari-jari tidak boleh kosong",
+                            Toast.LENGTH_LONG).show();
+                }else {
+                    String j = jari.getText().toString();
+                    double jari = Double.parseDouble(j);
+                    double hs = KelilingLingkaran(jari);
+                    String output = String.valueOf(hs);
+                    hasil.setText(output.toString());
+                }
+            }
+        });
     }
+    public double KelilingLingkaran(double jari) {return 2*3.14*jari;}
 }
